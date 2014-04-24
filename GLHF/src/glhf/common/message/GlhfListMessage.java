ab02593a636812964db5d47fa47fc1264b7d39d8
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2014-04-24 20:29:27 +0200

    GlhfListMessage javadoc.

--- GLHF/src/glhf/common/message/GlhfListMessage.java
+++ GLHF/src/glhf/common/message/GlhfListMessage.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 import crossnet.message.AbstractListMessage;
 
+/**
+ * These are used internally to maintain state. Has a List as a part of the payload.
+ * 
+ * @author Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
+ * 
+ * @param <T>
+ *            The list payload type.
+ */
 public abstract class GlhfListMessage< T > extends AbstractListMessage< T, GlhfMessageType > {
 
 	public GlhfListMessage( GlhfMessageType messageType, List< T > list ) {
